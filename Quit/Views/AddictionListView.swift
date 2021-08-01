f5e6cba8185b4c5839f9cb2fe21ddbfa46a1f98d
Grzegorz Gumieniak <kontaktg.gumieniak@gmail.com>
2021-08-01 21:40:33 +0200

    Change the way that is list showed. Using List instead of ScrollView for functionality as delete

--- Quit/Views/AddictionListView.swift
+++ Quit/Views/AddictionListView.swift
@@ -10,58 +10,61 @@ import SwiftUI
 
 struct AddictionListView: View {
     @StateObject private var addictionListVM: AddictionListViewModel = AddictionListViewModel()
+    @State private var showAlert: Bool = false
+    
     init() {
-        UINavigationBar.appearance().barTintColor = UIColor(named: "BackgroundMain")
-        UITabBar.appearance().barTintColor = UIColor(named: "BackgroundMain")
+        UITableView.appearance().backgroundColor = UIColor(named: "BackgroundMain")
     }
     var body: some View {
-        TabView {
-            NavigationView {
-                ZStack {
-                    Color("BackgroundMain").ignoresSafeArea(.all)
-                    ScrollView {
-                        VStack(alignment: .leading) {
-                            Text("Habbits").font(.title).bold().frame(alignment: .trailing).padding()
-                            ForEach(addictionListVM.addictions, id: \.id) { addiction in
-                                AddictionCellView(addiction: addiction).onTapGesture {
+        NavigationView {
+            ZStack {
+                Color("BackgroundMain").ignoresSafeArea(edges: .all)
+                VStack(alignment: .leading) {
+                    Text("GGumieniak")
+                        .font(.largeTitle)
+                        .bold()
+                        .padding(.leading)
+                    List {
+                        ForEach(addictionListVM.addictions, id: \.id) { addiction in
+                            AddictionCellView(addiction: addiction)
+                                .listRowBackground(Color("BackgroundMain"))
+                                .onTapGesture {
                                     addictionListVM.isShow = true
                                     addictionListVM.showView = addiction
                                 }
-                            }
-                        }
-                        NavigationLink("", destination: AddictionDetailView(detail: addictionListVM.showView?.name ?? "Invalid Request"), isActive: $addictionListVM.isShow)
-                    }
-                    VStack {
-                        Spacer()
-                        HStack {
-                            Spacer()
-                            NavigationLink(
-                                destination: AddNewAddictionView(),
-                                label: {
-                                    Image(systemName: "plus").foregroundColor(.white).font(.title)
+                                .alert(isPresented: self.$showAlert, content: {
+                                    Alert(title: Text("Hey"))
                                 })
-                                .frame(idealWidth: 50, maxWidth: 75, idealHeight: 50, maxHeight: 75, alignment: .center)
-                                .background(Color.green.opacity(0.8))
-                                .clipShape(Circle())
-                                .padding(25)
                         }
+                        .onDelete(perform: { indexSet in
+                            print("Delete")
+                        })
+                        .onHover(perform: { hovering in
+                            self.showAlert = true
+                        })
                     }
+                    .listStyle(PlainListStyle())
                 }
-                
-                .navigationBarTitle("")
-                .navigationBarHidden(true)
-            }.tabItem { Text("Habbits").buttonStyle(PlainButtonStyle()) }
-            Text("hahaha").tabItem { Text("Mateusz") }
+                NavigationLink("", destination: AddictionDetailView(detail: addictionListVM.showView?.name ?? "Invalid Request"), isActive: $addictionListVM.isShow)
+                VStack {
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        NavigationLink(
+                            destination: AddNewAddictionView(),
+                            label: {
+                                Image(systemName: "plus").foregroundColor(.white).font(.title)
+                            })
+                            .frame(idealWidth: 50, maxWidth: 75, idealHeight: 50, maxHeight: 75, alignment: .center)
+                            .background(Color.green.opacity(0.8))
+                            .clipShape(Circle())
+                            .padding(25)
+                    }
+                    
+                }
+            }
+            .navigationBarHidden(true)
         }
-        //        .toolbar {
-        //            ToolbarItemGroup(placement: .navigationBarLeading) {
-        //                NavigationLink(
-        //                    destination: AddNewAddictionView(),
-        //                    label: {
-        //                        Image(systemName: "plus").foregroundColor(.white).font(.title)
-        //                    })
-        //            }
-        //        }
         .onAppear(perform: addictionListVM.fetchData)
     }
 }
